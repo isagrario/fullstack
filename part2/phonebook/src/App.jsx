dd0331b6a92c3a12501b970f27f4b3a428bf8b74
isagrario@izar.net <isagrario@izar.net>
2025-12-11 13:09:14 +0100

    Ejercicios 2.12-2.15

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,8 +1,8 @@
 import { useState ,useEffect} from 'react'
-import axios from 'axios'
 import Person from './components/Person'
 import Filter from './components/Filter'
 import Form from './components/Form'
+import personService from './services/persons'
 
 
 const App = () => {
@@ -11,18 +11,31 @@ const App = () => {
   const [newNumber, setNewNumber] = useState('')
   const [newfilter, setNewFilter] = useState('')
   const [newPersonsFilter, setPersonsFilter] = useState('')
+  const [error, setError] = useState(null);
 
   const hook = () => {
-    console.log('effect')
-    axios
-      .get('http://localhost:3001/persons')
-      .then(response => {
-        console.log('promise fulfilled')
-        setPersons(response.data)
-      })
+    personService  
+      .getAll()
+      .then((initialPersons) => {
+      setPersons(initialPersons)
+    })
   }
 
 useEffect(hook, [])
+
+  const updateNumber = (id, number) => {
+      const person = persons.find(n => n.id === id)
+      const changedPerson = { ...person, number: number }
+  
+      personService      
+        .update(id, changedPerson)
+        .then(returnedPerson => {
+            setPersons(persons.map(person => person.id !== id ? person : returnedPerson))
+          })
+        .catch(error => {
+            setError(error)
+        })
+    }
 
   const addName = (event) => {
     //evita el modo por defecto
@@ -31,19 +44,46 @@ useEffect(hook, [])
     const nameObject = {
       name: newName, 
       number: newNumber,
-      id: persons.length + 1,    
+      id: String(persons.length + 1),    
     }
     /* console.log('button clicked', newName) */
     //comprobar si existe el nombre
     if(persons.find(person => person.name === newName)){
-      alert(`${newName} is already added to phonebook`)
+     
+      if(newNumber === ''){
+        alert(`${newName} ya existe, si quiere cambiar el Teléfono inserte uno nuevo`)
+        return
+      }
+      if (window.confirm(`¿Quiere cambiar el Teléfono de ${newName} ?`)){
+        //actualizar el número de la base de datos
+        const person = persons.find(person => person.name === newName)
+        updateNumber(person.id, newNumber)
+        //inicializar las listas
+        setPersonsFilter('')
+        setPersons(persons)
+        //limpiar el campo del filtro 
+        setNewFilter('')
+        //limpiar los campos del formulario
+        setNewName('')
+        setNewNumber('')
+     }
+
     } 
     else{
-      //agregar el nuevo nombre a la lista de persons
-      setPersons(persons.concat(nameObject))
-      //limpiar los campos del formulario
-      setNewName('')
-      setNewNumber('')  
+      //agregar el nuevo nombre a la base de datos
+      personService
+        .create(nameObject)
+        .then(returnedPerson => {
+          //agregar el nuevo nombre a la lista de persons
+          setPersons(persons.concat(returnedPerson))
+          //limpiar los campos del formulario
+          setNewName('')
+          setNewNumber('') 
+        })
+        .catch(error => {
+            setError(error)
+        })
+              
     }
   }
 
@@ -68,7 +108,7 @@ useEffect(hook, [])
   }
 
   const filterItems = (query) => {
-    //copiamos el objetop para aplicar el filtro
+    //copiar el objetop para aplicar el filtro
     const filterpersons = [...persons]
     return filterpersons.filter(person => person.name.toLowerCase().includes(query.toLowerCase()))
   }
@@ -81,27 +121,52 @@ useEffect(hook, [])
     setNewFilter('')
   }
 
+  const remove = (event) => {
+    event.preventDefault()
+    const id = event.target['id'].value
+    const person = persons.find(p => p.id === id)
+    if (window.confirm(`Delete ${person.name} ?`)) {
+      personService
+        .remove(id)
+        .then(() => {
+           //inicializar las listas
+          setPersonsFilter('')
+           //limpiar el campo del filtro 
+          setNewFilter('')
+          setPersons(persons.filter(p => p.id !== id))
+        })
+        .catch(error => {
+            setError(error);
+        });
+    }
+  }
+
   return (
     <div>
-      <h2>Phonebook</h2>
 
-      <Filter newfilter={newfilter} onChange ={handleFilterChange} refresh= {refresh} />
+      <h2>Phonebook</h2>
+      <Filter newfilter={newfilter} 
+              onChange ={handleFilterChange} 
+              refresh= {refresh} 
+      />
 
       <h2>Add a new</h2>
+          <Form addName ={addName} 
+                newName={newName} 
+                handleNameChange={handleNameChange} 
+                newNumber={newNumber} 
+                handleNumberChange={handleNumberChange}
+          />
 
-      <Form addName ={addName} 
-            newName={newName} 
-            handleNameChange={handleNameChange} 
-            newNumber={newNumber} 
-            handleNumberChange={handleNumberChange}
-      />
-       
       <div>
-      <h2>Numbers</h2>      
-         
-          <Person  persons={persons} personfilter={newPersonsFilter}/>        
-     
+      <h2>Numbers</h2>          
+          <Person persons={persons} 
+                  personfilter={newPersonsFilter} 
+                  remove={remove} 
+                  error={error}
+          />
       </div>
+
     </div>
   )
 }
--- part2/phonebook/src/components/Person.jsx
+++ part2/phonebook/src/components/Person.jsx
@@ -1,25 +1,36 @@
-const Person = ({ persons, personfilter }) => {
-  
+const Person = ({ persons, personfilter,remove ,error}) => {
+  //Mostrar error si existe
+  if(error){
+    return(
+      <div style={{color: 'red'}}>
+        {error.message}
+      </div>
+    )
+  }
+ //Si hay filtro, mostrar la lista filtrada
+  let personArray = []
   if (personfilter && personfilter.length > 0){
+    personArray = [ ...personfilter]
+  }
+  //Si no hay filtro, mostrar la lista completa
+  else {
+    personArray = [ ...persons]
+  }
     return(
       <ul>
-      {personfilter.map(person => 
+      {personArray.map(person => 
 
-            <li key={person.id} >{person.name} -- {person.number}</li>
+            <li key={person.id} >
+               {person.name} -- {person.number}
+              <form onSubmit={remove}>
+                <input type="hidden" name="id" value={person.id} /> 
+               <button type="submit" >delete</button>
+              </form>
+            </li>
+             
           )}
       </ul>
     ) 
   }
-  else {
-  return(
-    <ul>
-    {persons.map(person => 
-
-          <li key={person.id} >{person.name} -- {person.number}</li>
-        )}
-    </ul>
-  ) 
-  }
-}
 
 export default Person
